4950a495987889ab3de46ad5c06d50daae350878
Sergio <you@example.com>
2020-01-14 20:18:04 +0100

    queueu

--- ml_core/views.py
+++ ml_core/views.py
@@ -9,7 +9,7 @@ import time
 import paramiko
 import os
 import pickle
-import Queue
+import queue
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
