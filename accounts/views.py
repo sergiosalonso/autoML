8ca9d2ce0e7177d5cb6c7288f89fb970c3e4c5b8
Sergio <you@example.com>
2020-01-17 18:21:33 +0100

    new user

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth import login, authenticate
 from . import forms
 
 
 class SignUp(CreateView):
     form_class= forms.UserCreateForm
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('process:list-process')
     template_name = 'accounts/signup.html'
+    def form_valid(self, form):
+        to_return =super().form_valid(form)
+        user=authenicate(
+            username=form.cleaned_data["username"],
+            password=form.cleaned_data["password"],
+        )
+        login(self.request, user)
+        return to_return
